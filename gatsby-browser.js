89fe083e11f7e8a4285622dc8f4f389c62991677
Kang <rkdalsgur032@unist.ac.kr>
2023-12-26 17:52:11 +0900

    suspense intro completed

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,4 +7,6 @@ import "./src/normalize.css"
 import "./src/style.css"
 
 // Highlighting for code blocks
-import "prismjs/themes/prism.css"
+import "prismjs/themes/prism-okaidia.css"
+
+import "./src/code.css"
